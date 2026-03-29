c494b4773c5632f0f6c0bd24ece98bf2c9a311b5
nsoue <noah.souede@efrei.net>
2026-03-29 12:49:58 +0200

    correction bug read

--- automaton.py
+++ automaton.py
@@ -70,44 +70,43 @@ class Automaton:
 
 
 def read_txt(filename):
-    with open(filename,"r",encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         lines = [line.strip() for line in file if line.strip()]
-        #on retire les espaces et les sauts a la lines inutilles
+        # on retire les espaces et les sauts a la lines inutilles
+
     num_symbols = int(lines[0])
     num_states = int(lines[1])
-    #on lit le nombrez de symboles et d'états
+    # on lit le nombre de symboles et d'états
 
     initial_line = lines[2].split()
     num_initial_states = int(initial_line[0])
     initial_states = list(map(int, initial_line[1:1 + num_initial_states]))
-    #on lit le nombre d'états initiaux puis on établit la liste de ces derniers
+    # on lit le nombre d'états initiaux puis on établit la liste de ces derniers
 
     final_line = lines[3].split()
     num_final_states = int(final_line[0])
-    final_states = list(map(int, final_line[1:1 + num_final_states]))#même chose avec les états terminaux
+    final_states = list(map(int, final_line[1:1 + num_final_states]))
+    # même chose avec les états terminaux
 
     num_transitions = int(lines[4])
-    #on lit le nombres de transition
+    # on lit le nombres de transition
 
     transition_lines = lines[5:5 + num_transitions]
-    #on prend ici toutes les lignes a partir de la 5ème ligne en créant une liste pour en faire l'inventaire
+    # on prend ici toutes les lignes a partir de la 5ème ligne en créant une liste pour en faire l'inventaire
 
     transitions = []
 
     for transition in transition_lines:
-        i=0
-        while i < len(transition) and transition[i].isdigit():
-            i += 1
+        # On découpe la ligne en utilisant split() qui gère les espaces automatiquement
+        parts = transition.split()
 
-        start_state = int(transition[:i])
-        #on incrémente i jusqu'à qu'on tombe sur la lettre, et on séléctionne les chiffres présent avant pour extraire l'état de départ
+        if len(parts) >= 3:
+            start_state = int(parts[0])  #état de départ
+            symbol = parts[1]  #symbole associé a la transition
+            end_state = int(parts[2])  #état d'arrivé
 
-        symbol = transition[i]
-
-        end_state = int(transition[i + 1:])
-
-        transitions.append((start_state, symbol, end_state))
-        #on insère l'état de départ, le symbole associé a la transition e l'état d'arrivé
+            transitions.append((start_state, symbol, end_state))
+            # on insère l'état de départ, le symbole associé a la transition e l'état d'arrivé
 
     return Automaton(num_symbols, num_states, initial_states, final_states, transitions)
 
